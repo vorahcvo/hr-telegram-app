2440e981bd97cdec9826cd1cff346b3912c96060
vorahcvo <wowcharov@gmail.com>
2025-08-14 22:55:43 +0300

    Add loading spinner component

---
+++ src/components/UI/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadingSpinner: React.FC = () => {
+  return (
+    <div className="w-6 h-6 border-2 border-[#007aff] border-t-transparent rounded-full animate-spin"></div>
+  );
+};
+
+export default LoadingSpinner;
